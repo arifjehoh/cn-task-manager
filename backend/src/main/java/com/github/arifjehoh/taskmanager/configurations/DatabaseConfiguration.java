e6ada5b33da525e855a4cbb208a2258398cdd001
Arif Jehda-Oh <25460850+arifjehoh@users.noreply.github.com>
2024-11-17 13:29:04 +0100

    #1: Setting up (#16)
    * #1: create backend application
    
    - Spring Boot 3
    - Gradle
    - Java 23
    
    * ignore MAC file
    
    * #1: create frontend application
    
    - Deno 2
    - React with Vite
    - Typescript
    
    * #1: setting up sqlite database
    
    * #1: relative path for database
    
    - removed shared cache due to persistance database
    
    * #1: add persistance sqlite database volume
    
    * #1: add instructions how to run application localy

---
+++ backend/src/main/java/com/github/arifjehoh/taskmanager/configurations/DatabaseConfiguration.java
@@ -0,0 +1,31 @@
+package com.github.arifjehoh.taskmanager.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+    private final Environment env;
+
+    public DatabaseConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        String driverClassName = env.getProperty("spring.datasource.driver-class-name");
+        if (driverClassName == null) {
+            throw new RuntimeException("spring.datasource.driver-class-name is not set");
+        }
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+}
